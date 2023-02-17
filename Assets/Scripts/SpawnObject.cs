89702eb80a7fcc946a21446eb484a91e0e4b74f0
hunte <besscomdark@gmail.com>
2023-02-17 16:01:18 +0800

    製作遊戲畫面

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -11,9 +11,22 @@ namespace Lancelot
         public Vector3 center;
         public Vector3 size;
 
+        void Start()
+        {
+            SpawnFood();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Z))
+                SpawnFood();
+        }
+
         public void SpawnFood()
         {
             Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2),Random.Range(-size.z / 2, size.z / 2));
+
+            Instantiate(Foodprefab, pos, Quaternion.identity);
         }
 
         void OnDrawGizmosSelected()
