1ea571fb4f443d5213b6c8afc1b7b610e462ee11
hunte <besscomdark@gmail.com>
2023-03-24 07:01:35 +0800

    添加敵人腳本

---
+++ Assets/Scripts/DataEnemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lancelot
+{
+    [CreateAssetMenu(menuName = "Lancelot/Data Enemy", fileName = "Data Enemy")]
+    public class DataEnemy : ScriptableObject
+    {
+        [Header("¦å¶q"), Range(0, 10000)]
+        public float hp;
+        [Header("¶Ë®`"), Range(0, 10000)]
+        public float damage;
+    }
+}
